7089a35ef4539273020de459981f34da6866e2d5
谈亚健 <tanyajian@mincang.net>
2022-01-18 17:27:15 +0800

    no message

---
+++ YJCApp/YJCApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  YJCApp
+//
+//  Created by YJ-T on 2022/1/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world! hahaha")
+            .font(.title)
+            .foregroundColor(Color.green)
+            .padding()
+            
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YJCApp/YJCApp/YJCAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YJCAppApp.swift
+//  YJCApp
+//
+//  Created by YJ-T on 2022/1/18.
+//
+
+import SwiftUI
+
+@main
+struct YJCAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
